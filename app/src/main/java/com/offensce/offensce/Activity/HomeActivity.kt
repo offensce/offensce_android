c215514a43ec626a5444a3b65284f6d0ebd15dae
kunal8532 <kunalpasricha7@gmail.com>
2018-05-07 00:29:22 +0530

    First commit

---
+++ app/src/main/java/com/offensce/offensce/Activity/HomeActivity.kt
@@ -0,0 +1,13 @@
+package com.offensce.offensce.Activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.offensce.offensce.R
+
+class HomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
